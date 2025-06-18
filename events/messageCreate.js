8b026bff9170a62878bf06a06ae215d4e3fd40d0
PaloPil <71895868+PaloPil@users.noreply.github.com>
2025-06-18 22:10:19 +0200

    fix: Add criterias to suspect_input

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -70,7 +70,9 @@ function parse(str) {
 function suspect_input(str) {
   return (str.split("|").length > 100) || // >= 25 spoilers 
          (str.split("*").length > 100) ||
-         (str.split("_").length > 100);
+         (str.split("_").length > 100) ||
+         (str.split("`").length > 100) ||
+         (str.length > 1000);
 }
 
 
